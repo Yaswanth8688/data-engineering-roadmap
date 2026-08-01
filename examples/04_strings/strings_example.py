3b0d7d24e6139c20200523414c12e7294b2e5063
yaswanth <yaswanthmajji8688@gmail.com>
2026-08-01 19:13:58 +0530

    Add docstrings to Strings example

--- examples/04_strings/strings_example.py
+++ examples/04_strings/strings_example.py
@@ -1,4 +1,7 @@
-# Strings example
+"""Strings example.
+
+This module demonstrates string operations and Python docstrings.
+"""
 
 message = "Hello, Data Engineering!"
 name = 'Yaswanth'
@@ -19,3 +22,17 @@ print('Last 10 chars:', message[-10:])
 print(message.replace('Data', 'Python'))
 print(message.upper())
 print(message.lower())
+
+
+def greet(person_name: str) -> str:
+    """Return a greeting for the given person.
+
+    Args:
+        person_name: The name of the person to greet.
+
+    Returns:
+        A greeting string including the person's name.
+    """
+    return f"Hello, {person_name}!"
+
+print(greet(name))
